23a8397a8b6582c8e0de49bbdb3bb0c5a0996f02
Ramsey Dow <yesmar@gmail.com>
2017-12-22 17:19:15 -0800

    Fix git issue in release archives
    Fixes #2
    
    Release archives don't include the .git subdirectory, which is a
    dependency of config.mk. The fix zeros out $(commit) in the
    Makefile if the .git subdirectory is not found.

--- config.mk
+++ config.mk
@@ -1,5 +1,8 @@
-cmd?=dupes
-version?=1.0.0
-commit?=$(shell git rev-parse --short HEAD)
+cmd ?= dupes
+
+version ?= 1.0.1
+
+commit ?= $(shell git rev-parse --short HEAD 2>/dev/null)
+commit := $(if $(commit),+$(commit),)
 
 sources?=main.go
